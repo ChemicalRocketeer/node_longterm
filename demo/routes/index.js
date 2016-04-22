35eba1feaf9690a05b436a38136c9e457733f0cf
David Aaron Suddjian <aasuddjian@gmail.com>
2016-04-22 14:13:59 -0700

    Test longterm middleware in demo

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/longterm', function(req, res, next) {
+  console.log('longterm');
+  res.longterm();
+});
+
 module.exports = router;
