608c16560ebc20925b833ade42bfab1876ef0a79
David Aaron Suddjian <aasuddjian@gmail.com>
2016-04-23 16:22:18 -0700

    Fixing up MemoryQueue
    It was full of stuff that just straight up didn't work. :facepalm:

--- node_longterm/MemoryQueue.js
+++ node_longterm/MemoryQueue.js
@@ -1,21 +1,19 @@
 function MemoryQueue(options) {
   this._events = [];
-  this._eventMap = {};
   this._currentId = 0;
 }
 
-MemoryQueue.prototype.dequeue = function(callback) {
-  var evt = this._events[0];
-  this._events.splice(0, 1);
-  this._eventMap[evt.id] = undefined;
-  callback(null, evt);
+var proto = MemoryQueue.prototype;
+
+proto.next = function(callback) {
+  if (this._events.length > 0) {
+    callback(null, this._events[0]);
+  } else {
+    callback(null, null);
+  }
 };
 
-MemoryQueue.prototype.peek = function(callback) {
-  callback(null, this._events[0]);
-};
-
-MemoryQueue.prototype.enqueue = function(what, when, data, callback) {
+proto.enqueue = function(what, when, data, callback) {
   data = {
     id: this._currentId++,
     what: what,
@@ -29,44 +27,41 @@ MemoryQueue.prototype.enqueue = function(what, when, data, callback) {
     if (a.when > b.when) return 1;
     return 0;
   });
-  this._eventMap[data.id] = indexOfEvent(data.id, this._events);
   callback(null, data);
 };
 
-MemoryQueue.prototype.remove = function(id, callback) {
-  var index = this._eventMap[id];
+proto.remove = function(id, callback) {
+  var index = this._find(id);
   if (typeof index === 'undefined') {
     return callback(null, 0);
   }
   this._events.splice(index, 1);
-  this._eventMap[id] = undefined;
   callback(null, 1);
 };
 
-MemoryQueue.prototype.find = function(id, callback) {
-  var index = this._eventMap[id];
+proto.find = function(id, callback) {
+  var index = this._find(id);
   if (typeof index === 'undefined') {
     return callback(null, null);
   }
   callback(this._events[index]);
 };
 
-MemoryQueue.prototype.count = function(callback) {
+proto.count = function(callback) {
   callback(null, this._events.length);
 };
 
-MemoryQueue.prototype.clear = function(callback) {
+proto.clear = function(callback) {
   var count = this._events.length;
   this._events = [];
-  this._eventMap = {};
   callback(null, count);
 };
 
-function indexOfEvent(eventId, arr) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i].id === eventId) return i;
+proto._find = function(id) {
+  for (var i = 0; i < this._events.length; i++) {
+    if (this._events[i].id === id) return i;
   }
   return -1;
-}
+};
 
 module.exports = MemoryQueue;
