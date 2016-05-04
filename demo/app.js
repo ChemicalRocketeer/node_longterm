0d3c1cd615693e1c272586f3accf883eeb61ba53
David Aaron Suddjian <aasuddjian@gmail.com>
2016-05-03 17:39:46 -0700

    'index.js' -> 'longterm.js'

--- demo/app.js
+++ demo/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var longterm = require('../node_longterm/index');
+var longterm = require('../node_longterm/longterm');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
