31aa45bc6f38d4481859180d15f1de654409f4f0
David Aaron Suddjian <aasuddjian@gmail.com>
2016-05-07 15:59:44 -0700

    Some test stuff

---
+++ node_longterm/test/longterm.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var longterm = require('../longterm');
+var async = require('async');
+
+describe('longterm', function() {
+
+
+  describe('middleware', function() {
+    beforeEach(function(done) {
+    });
+  });
+});
