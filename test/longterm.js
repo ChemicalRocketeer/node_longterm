af9f3feb0a1c9e0e4e5a076aa197909b153ce034
David Aaron Suddjian <aasuddjian@gmail.com>
2016-08-13 14:58:30 -0700

    testing

--- test/longterm.js
+++ test/longterm.js
@@ -1,12 +1,26 @@
 var assert = require('assert');
+var expect = require('chai').expect;
 var longterm = require('../longterm');
 var async = require('async');
 
 describe('longterm', function() {
 
-
-  describe('middleware', function() {
-    beforeEach(function(done) {
+  beforeEach(function(done) {
+    longterm.clear(function() {
+      longterm.init();
+      done();
     });
-  });
+  })
+
+  it('should hide the emit function', function(done) {
+    var EventEmitter = require('events');
+    expect(longterm).to.not.have.property('emit');
+    expect(longterm.prototype).to.not.have.property('emit');
+    longterm.on('test', function() {
+      expect(this).to.have.property('on');
+      expect(this).to.not.have.property('emit');
+      done();
+    })
+    longterm('test', Date.now());
+  })
 });
